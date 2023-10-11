1f72758a92f82585f18ca16256497cb138664230
indrajeet sriwastav <121600838+INDRAJEET-SRIWASTAV@users.noreply.github.com>
2023-10-11 18:48:06 +0000

    Conversion of Data Types

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+const score = "33adf"
+
+// console.log(typeof score)
+
+
+const valueInnumber = Number(score)
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+
+
+
+// when we convert "33" => converted into 33
+// when we conver "33abc" => converted into NaN
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// when we convert 1 => converted into true
+// when we convert "" => converted into false
+// when we convert "indrajeet" => converted into true
